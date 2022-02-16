9c9a8fcad41a584b9b993af8c358dce67223c226
Endrit Hyseni <pixendrit@outlook.com>
2022-02-16 17:41:26 +0100

    trying to set debugging to false

--- codingjobs/settings.py
+++ codingjobs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = '+16a2x$b1$h1wv2tw#lcy-%wwvf*7b$j%vy-(-$qoc@gp@l7r6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
