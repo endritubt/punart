7f00a373c6ee96b2c7cfb5d2dca255df4d0d04ef
Pixendrit <pixendrit@outlook.com>
2022-02-18 01:37:34 +0100

    testing

--- codingjobs/settings.py
+++ codingjobs/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aq/xFCeIzNvbcp2OZkG8RUIT2PuTbI1BqaX8+XY5SCc='
+#SECRET_KEY = 'aq/xFCeIzNvbcp2OZkG8RUIT2PuTbI1BqaX8+XY5SCc='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
